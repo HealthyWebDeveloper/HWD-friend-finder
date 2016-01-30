e06fa2e5eba7b4525ac4554e28f321ca6ccb3c5c
Bradford Knowlton <brad@ucanbonline.net>
2016-01-30 15:51:34 -0700

    working shortcode, needs mroe styling

--- includes/class-friend-shortcode.php
+++ includes/class-friend-shortcode.php
@@ -21,7 +21,20 @@ class Friend_Shortcode {
 
 	    query_posts(array( 
 	        'post_type' => 'friend',
-	        'showposts' => $atts['per_page'] 
+	        'showposts' => $atts['per_page'],
+	        'meta_query' => array(
+	        						'relation' => 'AND',
+					        		array(
+				                        'key' => 'avatar150',
+				                        'value' => 'https://static0.fitbit.com/images/profile/defaultProfile_100_male.gif',
+				                        'compare' => 'NOT LIKE',
+				                    ),
+				                    array(
+				                        'key' => 'avatar150',
+				                        'value' => 'https://static0.fitbit.com/images/profile/defaultProfile_100_female.gif',
+				                        'compare' => 'NOT LIKE',
+				                    ),
+					        	)
 	    ) );  
 
 	    if (have_posts()):
@@ -31,7 +44,7 @@ class Friend_Shortcode {
 		 		$displayName = get_post_meta( get_the_id(), 'displayName', true );
 		 		$avatar = get_post_meta( get_the_id(), 'avatar150', true );
 
-		        $return .=  sprintf('<div class="small-12 medium-4 columns"><p><h2>%s</h2><img src="%s" />%s</p></div>', $displayName, $avatar, get_the_content() );
+		        $return .=  sprintf('<div class="hwd-profile-list-item small-12 medium-4 columns"><img src="%s" /><h2>%s</h2><p class="hide">%s</p></div>', $displayName, $avatar, get_the_content() );
 		     endwhile;
 	     
 	     else:
