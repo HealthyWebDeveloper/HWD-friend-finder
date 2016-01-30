9445d8f6be68429beef2e153070841018e4f3711
Bradford Knowlton <brad@ucanbonline.net>
2016-01-30 12:01:45 -0700

    Initial Commit

---
+++ hwd-friend-finder.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: HWD Friend Finder
+Description: Adds custom post type and fun stuff for Fibit tools on a WordPress site.
+Plugin URI: http://healthywebdeveloper.com
+Author: Bradford Knowlton
+Author URI: http://bradknowlton.com
+Version: 1.0.1
+License: GPL2
+Text Domain: hwd
+Domain Path: /languages
+*/
+
+/*
+
+    Copyright (C) 2016  Bradford Knowlton brad@healthywebdeveloper.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
---
+++ readme.txt
@@ -0,0 +1,13 @@
+=== Plugin Name ===
+Contributors: bradmkjr
+Donate link: http://bradknowlton.com
+Tags: Fitbit, friend finder
+Requires at least: 4.4
+Tested up to: 6.0.1
+Stable tag: master
+
+Adds custom post type and fun stuff for Fibit tools on a WordPress site.
+
+== Description ==
+
+Blah
