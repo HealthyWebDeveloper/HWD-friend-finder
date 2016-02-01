f8234a63abbeabc1e1b62a08afe5d94436c41501
Bradford Knowlton <brad@ucanbonline.net>
2016-01-31 23:21:57 -0700

    added success message

--- includes/templates/settings.php
+++ includes/templates/settings.php
@@ -9,15 +9,9 @@
 
 <div class="wrap">
     <form method="post"> 
-        <h2>Add Friend Profile</h2>
-        Easily create new profile with API and Fitbit ID
-        <table class="form-table">
-            <tbody>
-              <tr><th scope="row"><?php _e( 'Fitbit ID', 'hwd' ); ?></th><td><input type="text" placeholder="3JDKLR" value="" id="encodedId" name="encodedId"></td></tr>
-              
-            </tbody></table>
-            <p class="submit"><input type="submit" value="Create Profile" class="button button-primary" id="submit" name="submit"></p>
-    </form>
+        <h2><?php _e( 'Add Friend Profile', 'hwd' ); ?></h2>
+        <?php _e( 'Easily create new profile with API and Fitbit ID', 'hwd' ); ?>
+        
 
     <?php
     if( isset( $_POST['encodedId'] ) && '' != $_POST['encodedId'] ){
@@ -31,28 +25,46 @@
 
       $profile = $fitbit_php->getProfile();
 
-      // var_dump($profile->user->encodedId);
-
       // Create post object
       $my_post = array(
         'post_title'    => $profile->user->encodedId,
         'post_content'  => $profile->user->encodedId,
         'post_status'   => 'publish',
-        'post_author'   => 1,
-        // 'post_category' => array( 8,39 ),
         'post_type'     => 'friend'
       );
        
       // Insert the post into the database
-      $post_id = wp_insert_post( $my_post );
+      $post_id = wp_insert_post( $my_post, true );
+
+      if( ! is_wp_error( $post_id ) ) {
 
       foreach($profile->user  as $meta_key => $meta_value){
         add_post_meta($post_id, $meta_key, $meta_value, true);
       }
 
+      ?>
+          <div id="message" class="updated">
+              <p><strong><?php _e('Friend Added.', 'hwd') ?></strong></p>
+          </div>
+      <?php 
+      }else{
+        ?>
+          <div id="message" class="error">
+              <p><strong><?php _e('Friend Create Failed.', 'hwd') ?></strong></p>
+          </div>
+      <?php 
+      }
+
     }
 
     ?>
+    <table class="form-table">
+            <tbody>
+              <tr><th scope="row"><?php _e( 'Fitbit ID', 'hwd' ); ?></th><td><input type="text" placeholder="3JDKLR" value="" id="encodedId" name="encodedId"></td></tr>
+              
+            </tbody></table>
+            <p class="submit"><input type="submit" value="Create Profile" class="button button-primary" id="submit" name="submit"></p>
+    </form>
 </div>
 
 
