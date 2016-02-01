5d235a17a02ba0001e06253557c65a61832d9577
Bradford Knowlton <brad@ucanbonline.net>
2016-01-31 21:07:27 -0700

    added settings class

--- hwd-friend-finder.php
+++ hwd-friend-finder.php
@@ -5,7 +5,7 @@ Description: Adds custom post type and fun stuff for Fibit tools on a WordPress 
 Plugin URI: http://healthywebdeveloper.com
 Author: Bradford Knowlton
 Author URI: http://bradknowlton.com
-Version: 1.2.1
+Version: 1.3.1
 License: GPL2
 Text Domain: hwd
 Domain Path: /languages
@@ -38,7 +38,7 @@ Requires WP: 4.4
 // Help with setting up gulp
 // https://travismaynard.com/writing/getting-started-with-gulp
 
-define( 'HWD_PLUGIN_VERSION', '1.2.1' );  
+define( 'HWD_PLUGIN_VERSION', '1.3.1' );  
 
 // possible future global
 // define( 'PLUGIN_DIR', dirname(__FILE__).'/' );  
@@ -48,6 +48,7 @@ define( 'HWD_PLUGIN_VERSION', '1.2.1' );
 require_once( plugin_dir_path( __FILE__ ) . 'includes/class-fitbit-api.php');
 require_once( plugin_dir_path( __FILE__ ) . 'includes/class-friend-shortcode.php');
 require_once( plugin_dir_path( __FILE__ ) . 'includes/class-friend-post-type.php');
+require_once( plugin_dir_path( __FILE__ ) . 'includes/class-friend-settings.php');
 
 
 /**
---
+++ includes/class-friend-settings.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Class for Friend Settings
+ *
+ * @since 1.2.1
+ *
+ * @package hwd-friend-finder
+ */
+
+class Friend_Settings {
+
+
+
+} // end Friend_Settings Class
+
+
